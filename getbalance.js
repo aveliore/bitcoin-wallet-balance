0e2d0acc49e2e960e25cb0c096c8baabb12b6ce8
azagdanski <andrzej.zagdanski@aveliore.com>
2022-11-19 23:56:51 +0100

    Testing

--- getbalance.js
+++ getbalance.js
@@ -1,3 +1,12 @@
+function onClick($this) {
+    var value = $this.previousElementSibling.value;
+    if(value == ''){
+        console.log('no input');
+    }else{
+       console.log(value);
+    }
+  }
+
 function getBalance() {
     var input = document.getElementById("bwa").value;
     alert(input);
