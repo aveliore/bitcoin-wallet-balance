bc560813fcc65f8e361de5a5e5eb4481c1eb12fa
azagdanski <andrzej.zagdanski@aveliore.com>
2022-11-20 00:29:36 +0100

    Fix content-type erro

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html> 
 <html lang="pl">
     <head>
-        <meta charset="UTF-8">
+        <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Check Bitcoin Wallet Balance | BWB</title> 
