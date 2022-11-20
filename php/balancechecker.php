2fe855b58ab26218d2e9cd0f4bf39e20fbae4d77
azagdanski <andrzej.zagdanski@aveliore.com>
2022-11-20 01:40:50 +0100

    Remove PHP files

--- php/balancechecker.php
+++
@@ -1,5 +0,0 @@
-<?php
-function getBalance($address) {
-    return file_get_contents('https://blockchain.info/q/addressbalance/'. $address);
-}
-echo 'Address Balance: ' . getBalance('1EzwoHtiXB4iFwedPr49iywjZn2nnekhoj');
