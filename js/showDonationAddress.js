5ddddd9b37c612721bb032332d45d87d62561441
azagdanski <andrzej.zagdanski@aveliore.com>
2022-11-21 03:48:19 +0100

    Testing changes

--- js/showDonationAddress.js
+++ js/showDonationAddress.js
@@ -1,6 +1,6 @@
 function showDonationAddress() {
     var x = document.getElementById("donationAddress");
-    if (x.innerHTML === "&#8205;") {
+    if (x.innerHTML === "&#8205; HERE") {
       x.innerHTML = "Swapped text!";
     } else {
       x.innerHTML = "&#8205;";
