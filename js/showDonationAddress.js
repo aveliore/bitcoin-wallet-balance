c1357b8fdef98f96ac94e55354f54beee9e2e7ea
azagdanski <andrzej.zagdanski@aveliore.com>
2022-11-21 03:02:35 +0100

    Add donation wallet address button and viewer

---
+++ js/showDonationAddress.js
@@ -0,0 +1,8 @@
+function showDonationAddress() {
+    var x = document.getElementById("donationAddressText");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
