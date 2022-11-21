90e4260ea61304f088e4bb388bc69ef8ab2cf3b5
azagdanski <andrzej.zagdanski@aveliore.com>
2022-11-21 03:29:50 +0100

    JS changes

--- js/showDonationAddress.js
+++ js/showDonationAddress.js
@@ -1,3 +1,2 @@
-function showDonationAddress(){
-    document.getElementById("donationAddressText").textContent = "bc1qsdy4js4w987vtujj4sm44n2jzxkc2ucuj07c8j";
-}
+var element = document.getElementById("donationAddressText); 
+element.innerHTML("New content"); 
