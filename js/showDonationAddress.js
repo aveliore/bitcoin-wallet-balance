754e8f4b177b10ab34800b9ab2ae9d74a0f628ca
azagdanski <andrzej.zagdanski@aveliore.com>
2022-11-21 03:23:39 +0100

    Change innerHTML to textContent

--- js/showDonationAddress.js
+++ js/showDonationAddress.js
@@ -1,3 +1,3 @@
 function showDonationAddress(){
-    document.getElementById("donationAddressText").innerHTML = "bc1qsdy4js4w987vtujj4sm44n2jzxkc2ucuj07c8j";
+    document.getElementById("donationAddressText").textContent = "bc1qsdy4js4w987vtujj4sm44n2jzxkc2ucuj07c8j";
 }
