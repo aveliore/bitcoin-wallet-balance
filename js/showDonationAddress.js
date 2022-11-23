43dcc98fb432936f92b2c10584d32914c303aa89
azagdanski <andrzej.zagdanski@aveliore.com>
2022-11-23 04:07:04 +0100

    Styling donation address viewer

--- js/showDonationAddress.js
+++ js/showDonationAddress.js
@@ -1,8 +1,8 @@
 function showDonationAddress() {
     var x = document.getElementById("donationAddress");
     if (x.innerHTML === "") {
-      x.innerHTML = "Swapped text!";
+      x.innerHTML = "<span class='donationAddressText' bc1qsdy4js4w987vtujj4sm44n2jzxkc2ucuj07c8j</span>";
     } else {
-      x.innerHTML = "Swapped text!";
+      x.innerHTML = "<span class='donationAddressText' bc1qsdy4js4w987vtujj4sm44n2jzxkc2ucuj07c8j</span>";
     }
   }
