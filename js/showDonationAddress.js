5f1743f9e1480deec1a8faa88d1e14bdd2bf0800
azagdanski <andrzej.zagdanski@aveliore.com>
2022-11-23 04:09:59 +0100

    Minor error fix

--- js/showDonationAddress.js
+++ js/showDonationAddress.js
@@ -1,8 +1,8 @@
 function showDonationAddress() {
     var x = document.getElementById("donationAddress");
     if (x.innerHTML === "") {
-      x.innerHTML = "<span class='donationAddressText' bc1qsdy4js4w987vtujj4sm44n2jzxkc2ucuj07c8j</span>";
+      x.innerHTML = "bc1qsdy4js4w987vtujj4sm44n2jzxkc2ucuj07c8j";
     } else {
-      x.innerHTML = "<span class='donationAddressText' bc1qsdy4js4w987vtujj4sm44n2jzxkc2ucuj07c8j</span>";
+      x.innerHTML = "bc1qsdy4js4w987vtujj4sm44n2jzxkc2ucuj07c8j";
     }
   }
